7bfdd7c5c02b71b61cd228fa611ababf06d43036
Jean Patrick Hartmann <32403781+hartmannjean@users.noreply.github.com>
2024-09-19 00:12:36 -0300

    Added new fields on ui components

---
+++ app/src/main/java/com/example/engsftwgraph/ui/components/NameField.kt
@@ -0,0 +1,17 @@
+package com.example.engsftwgraph.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NameField(name: String, onValueChange: (String) -> Unit) {
+    OutlinedTextField(
+        value = name,
+        onValueChange = onValueChange,
+        label = { Text("Name") },
+        modifier = Modifier.fillMaxWidth()
+    )
+}
