5b7d8004c919dabec1f8e0f69727371293fc497b
Jean Patrick Hartmann <32403781+hartmannjean@users.noreply.github.com>
2024-09-20 19:21:14 -0300

    Change on ui components for SigUp Created validateFields

--- app/src/main/java/com/example/engsftwgraph/view/SignUp.kt
+++ app/src/main/java/com/example/engsftwgraph/view/SignUp.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.engsftwgraph.ui.components.AccountField
@@ -15,6 +16,8 @@ import com.example.engsftwgraph.ui.components.EmailField
 import com.example.engsftwgraph.ui.components.NameField
 import com.example.engsftwgraph.ui.components.PasswordField
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.engsftwgraph.navigation.AppRoutes
+import com.example.engsftwgraph.ui.components.TitleText
 import com.example.engsftwgraph.viewModel.SignUpViewModel
 
 @Composable
@@ -22,6 +25,7 @@ fun SignUpScreen(
     navController: NavController, viewModel: SignUpViewModel = viewModel()
 ) {
 
+    val context = LocalContext.current
     var name by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var accountnumber by remember { mutableStateOf("") }
@@ -30,10 +34,13 @@ fun SignUpScreen(
     var isLoading by remember { mutableStateOf(false) }
     var showSnackbar by remember { mutableStateOf(false) }
     var snackbarMessage by remember { mutableStateOf("") }
+    val snackbarHostState = remember { SnackbarHostState() }
 
     Scaffold(snackbarHost = {
         if (showSnackbar) {
-            CustomSnackbar(message = snackbarMessage, onDismiss = { showSnackbar = false })
+            CustomSnackbar(snackbarHostState, snackbarMessage) {
+                snackbarMessage = ""
+            }
         }
     }) { paddingValues ->
         Column(
@@ -44,7 +51,7 @@ fun SignUpScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterVertically)
         ) {
-            Text(text = "Criar Conta", style = MaterialTheme.typography.headlineMedium)
+            TitleText(text = "Criar Conta")
 
             NameField(name) { name = it }
 
@@ -52,27 +59,35 @@ fun SignUpScreen(
 
             EmailField(email) { email = it }
 
-            PasswordField(password) { password = it }
+            PasswordField(password = password,
+                { password = it },
+            )
 
-            PasswordField(confirmPassword) { confirmPassword = it }
+            PasswordField(password = confirmPassword,
+                { confirmPassword = it },
+                label = "Repetir senha"
+            )
 
             CreateAccountButton(isLoading = isLoading, onClick = {
-                if (email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
-                    snackbarMessage = "Por favor, preencha todos os campos."
+                val errorMessage = viewModel.validateFields(name, email, accountnumber, password, confirmPassword)
+                if (errorMessage != null) {
+                    snackbarMessage = errorMessage
                     showSnackbar = true
                     return@CreateAccountButton
                 }
 
                 isLoading = true
-                viewModel.createUser(name = name,
+                viewModel.createUser(
+                    context = context,
+                    name = name,
                     email = email,
                     accountNumber = accountnumber,
                     password = password,
                     confirmPassword = confirmPassword,
                     onSuccess = {
                         isLoading = false
-                        navController.navigate("home") {
-                            popUpTo("signup") {
+                        navController.navigate(AppRoutes.LOGIN) {
+                            popUpTo(AppRoutes.SIGNUP) {
                                 inclusive = true
                             }
                         }
